19209d3f8df459ea1e97c15d5bd04d487cba12fd
palNisarga <61311493+palNisarga@users.noreply.github.com>
2021-04-28 10:30:17 +0530

    Update ClientConfig.java

--- Source/ClientConfig.java
+++ Source/ClientConfig.java
@@ -1,7 +1,27 @@
-package ParkingLot;
+package ParkingLot; 
+import java.io.FileReader;
 import java.sql.*;
-public class ClientConfig 
-{  
+import java.util.Properties;
+import java.util.Scanner;
+
+public class ClientConfig {
+    public static Connection connection;
+    public static Statement statement;
+    public static Properties properties;
+    @SuppressWarnings("unused")
+	private static String client;
+
+
+    public void fileConfig() {
+        try {
+            properties = new Properties();
+            properties.load(new FileReader("src/main/resources/config.properties"));
+            MySQL.client = properties.getProperty("Client");
+        } catch (Exception e) {
+                 e.printStackTrace();
+        }
+
+}
 
 public Connection create() {
 	Connection con= null;
@@ -16,4 +36,12 @@ public Connection create() {
 		}
 	return con;
 }
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void setConnection(Connection connection) {
+		MySQL.connection = connection;
+	}
+
 }
