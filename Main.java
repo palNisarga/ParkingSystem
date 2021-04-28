656fd41568ce163543713e0fd1c8d5a73a19b2fe
palNisarga <61311493+palNisarga@users.noreply.github.com>
2021-04-28 09:08:05 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,36 +10,40 @@ public class Main {
 
 	public static void main(String[] args) throws IOException, SQLException {
 		Object client=new InMemoryClient();
-		
+		String FetchData = input.nextLine();
+		Object database;
+		if(FetchData.equalsIgnoreCase("inMemory")) {
+			else {
+				  database = new MysqlDB();
 
 		Scanner input = new Scanner(System.in);
-	    int menu = 0;
+	    	int menu = 0;
 		do {
 			Menu.options();
 			menu = input.nextInt();
       
 		switch(menu) {
 			case(1):
-				MySqlClient.SetSlot();
+				database.SetSlot();
 				break;
 			case(2):
-				MySqlClient.ToRemoveFromSlot();
+				database.ToRemoveFromSlot();
 				break;
 			case(3):
-				MySqlClient.SearchSlotByRegNumber();
+				database.SearchSlotByRegNumber();
 				break;
-            case(4):
-                MySqlClient.SearchCarByColor();
+           		 case(4):
+              			  database.SearchCarByColor();
 				break;
-            case(5):
-            	MySqlClient.searchSlotByColor();//
+           		 case(5):
+            			database.searchSlotByColor();//
 				break;
 			case(0):
-                System.out.println("\nThank you!\n");
-	            break;
-            default: {
-	             System.out.println("Invalid option!\n");
-	         //break;
+               			 System.out.println("\nThank you!\n");
+	           		 break;
+            		default: {
+	            		 System.out.println("Invalid option!\n");
+	         		//break;
 	                    }
 		      }	
 		}
