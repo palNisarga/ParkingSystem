68f7b0c4736f4bcb71f96496ec1e7e524f725e5a
palNisarga <61311493+palNisarga@users.noreply.github.com>
2021-04-30 09:52:47 +0530

    Update InMemoryClient.java

--- client/InMemoryClient.java
+++ client/InMemoryClient.java
@@ -10,11 +10,11 @@ import java.util.Scanner;
 
 public class InMemoryClient extends BaseClient {
 	
-		ArrayList<Integer> Avails = new ArrayList<Integer>();
-		List<Integer> Occupied = new ArrayList<Integer>();
-		Scanner input = new Scanner(System.in);
-		Hashtable<Integer, List<String>> Level = new Hashtable<Integer, List<String>>();
-		public InMemoryClient() {
+	ArrayList<Integer> Avails = new ArrayList<Integer>();
+	List<Integer> Occupied = new ArrayList<Integer>();
+	Scanner input = new Scanner(System.in);
+	Hashtable<Integer, List<String>> Level = new Hashtable<Integer, List<String>>();
+	public InMemoryClient() {
 	    Collections.addAll(Avails, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
 	    101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,
 	    201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,
@@ -23,14 +23,14 @@ public class InMemoryClient extends BaseClient {
 		}
 	    public void SetSlot() {
 	    
-		    int Menu;
-			String floor = " ";
+		int menu;
+		String floor = " ";
 	        List<String> array = new ArrayList<String>(10);
 	        Date date = new Date();
 	                SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy,E HH:mm:ss");
 	                String strDate = formatter.format(date);
 	               
-	        System.out.println("Enter Colour");
+	        	System.out.println("Enter Colour");
 	                String colour = input.next();
 	               
 	                System.out.println("Enter Reg Plate");
@@ -80,9 +80,9 @@ public class InMemoryClient extends BaseClient {
 				System.out.println("No cars in the slot!");
 			}else {
 				System.out.println("Enter Slot no");	
-	        Scanner input = new Scanner(System.in);
+	        	Scanner input = new Scanner(System.in);
 			Integer slot_no = input.nextInt();
-		    for( Map.Entry<Integer, List<String>> entry : Level.entrySet() )
+		   	for( Map.Entry<Integer, List<String>> entry : Level.entrySet() )
 		        {
 		            Integer key = entry.getKey();
 		            List<String> values = entry.getValue();
@@ -137,7 +137,7 @@ public class InMemoryClient extends BaseClient {
 	    	}
 			
 		}
-		public void searchSlotByColor() {
+		public void SearchCarByColor() {
 			System.out.println("Enter the color");
 	    	String Clr= input.next();
 	    	int count=0;
