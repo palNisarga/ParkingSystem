b54bb0a980a34671d6e52250839177d84c42d857
palNisarga <61311493+palNisarga@users.noreply.github.com>
2021-05-04 09:23:10 +0530

    Update MongoClient.java

--- src/client/MongoClient.java
+++ src/client/MongoClient.java
@@ -1,58 +1,54 @@
 package parkingLot;
 import java.util.ArrayList;
-import java.util.Collections;
-import com.mongodb.MongoClient;
-import com.mongodb.BasicDBObject;
-import com.mongodb.client.MongoCollection;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
+import org.bson.Document;
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 
-	public class MongoClient extends BaseClient  {
-     static Scanner input = new Scanner(System.in);
-	   static MongoDB mongo =new MongoDB();
-	   static Connection con = mongo.create();
-	   public void SetSlot() throws SQLException {
-       	System.out.println("Enter Colour");
-	    	String colour = input.next();
-	    	System.out.println("Enter Reg Plate");
-	    	String regPlate = input.next();
-	      
-	    	Statement stmt= con.createStatement();
-        Vehicle vehicle=registerVehicle();
-        boolean flag=false;
-        if(vehicle!=null && !duplicateInfo(vehicle)) {
-            appConfig.getUpdateFields().append("registrationNumber", vehicle.getRegNo().toUpperCase());
-            appConfig.getUpdateFields().append("Colour", vehicle.getColour().toUpperCase());
-            appConfig.getSetQuery().append("$set", appConfig.getUpdateFields());
-            appConfig.getCollection().updateOne(Filters.eq("registrationNumber", null), appConfig.getSetQuery());
-            displayTicket(vehicle.getRegNo().toUpperCase());
-            flag=true;
-        }
-       if(!flag){
-            System.out.println();
-            System.out.println("Wrong Input");
-        }
+public class MongoClient extends BaseClient {
+   static Scanner input = new Scanner(System.in);
+   static ClientConfig mongodb=new ClientConfig();
+   static MongoClient con = mongodb.mongoConnection();
+   static MongoDatabase database = con.getDatabase("myDatabase");
+
+	public void SetSlot() {
+		System.out.println("Enter Colour");
+    		String colour = input.next();
+    		System.out.println("Enter Reg Plate");
+    		String regPlate = input.next();
+    	
+    		System.out.println("Alloting a slot..."+"\n");
+    		boolean flag=false;
+    		Document document = new Document();
+    	
+    		document.append("registrationNumber", regPlate);
+    		document.append("Colour", colour);
+    		database.getCollection("vehicles").insertOne(document);
+	}
        Document vehicleInfo = appConfig.getCollection().find(new Document("registrationNumber", regNo)).first();
        System.out.println("Alloting a slot... " + vehicleInfo.get("_id"));
      }
     
      public void ToRemoveFromSlot(){
-	      System.out.println("Enter Reg Plate");
-        String regNo=sc.nextLine().toUpperCase();
-        appConfig.getUpdateFields().append("registrationNumber",null);
-        appConfig.getUpdateFields().append("Colour", null);
+	System.out.println("Enter Reg Plate");
+        String regNo=sc.nextLine());
+        document.append.append("registrationNumber",null);
+        document.append.append("Colour", null);
         BasicDBObject setQuery = new BasicDBObject();
-        setQuery.append("$set", appConfig.getUpdateFields());
+        setQuery.append("$set", insertOne(document));
         appConfig.getCollection().updateOne(Filters.eq("registrationNumber",regNo), setQuery);
-        System.out.println("Slot is now available ");
+        
 	    }
     
     public void SearchSlotByRegNumber() {
         boolean flag=false;
         System.out.print("Enter vehicle Registration Number :");
-        String regNo=sc.nextLine().toUpperCase();
+        String regNo=sc.nextLine();
         for (Document info : vehicle) {
             System.out.println("vehicle Slot Number :"+info.get("_id"));
             flag=true;
@@ -66,8 +62,7 @@ import java.util.Scanner;
     public void SearchCarByColor() {
         boolean flag=false;
         System.out.print("Enter vehicle Colour :");
-        String colour=sc.nextLine().toUpperCase();
-        List<Document> vehicleInfo = appConfig.getCollection().find(Filters.eq("Colour", colour)).into(new ArrayList<>());
+        String colour=sc.nextLine(); 
         for (Document info : vehicleInfo) {
             System.out.println("vehicle Registration Number :"+info.get("registrationNumber"));
             flag=true;
@@ -81,8 +76,7 @@ import java.util.Scanner;
     public void SearchSlotByColor() {
         boolean flag=false;
         System.out.print("Enter vehicle Colour :");
-        String colour=sc.nextLine().toUpperCase();
-        List<Document> slotInfo = appConfig.getCollection().find(Filters.eq("Colour", colour)).into(new ArrayList<>());
+        String colour=sc.nextLine();
         for (Document info : slotInfo) {
             System.out.println("Vehicle Slot Number  : " + info.get("_id"));
             flag=true;
